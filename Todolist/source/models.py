f231743dc40bb805e73e65cc749f5ab69081ffab
aderty123 <77917326+aderty123@users.noreply.github.com>
2024-04-21 19:58:29 +0500

    Add files via upload

---
+++ Todolist/source/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'Новая'),
+        ('in_progress', 'В процессе'),
+        ('done', 'Сделано'),
+    ]
+    description = models.TextField(verbose_name='Описание')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new', verbose_name='Статус')
+    due_date = models.DateField(blank=True, null=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return self.description
---
+++ Todolist/todo_list/settings.py
@@ -0,0 +1,90 @@
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+# Application definition
+
+INSTALLED_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "todolist",
+    "lists",
+    "accounts",
+    "rest_framework",
+    "api",
+)
+
+MIDDLEWARE = (
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+)
+
+ROOT_URLCONF = "todolist.urls"
+
+WSGI_APPLICATION = "todolist.wsgi.application"
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
+
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+
+STATIC_URL = "/static/"
+
+
+LOGIN_URL = "/auth/login/"
+
+LOGOUT_URL = "/auth/logout/"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.request",
+            ],
+            "debug": True,
+        },
+    },
+]
